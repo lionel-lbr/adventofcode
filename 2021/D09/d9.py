5410714dec14957fb58692ac9049e26bfaaf0a10
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-01-09 18:26:07 +0800

    Advent Of Code 2021 Day 9 part 1 & 2

--- 2021/D09/d9.py
+++ 2021/D09/d9.py
@@ -4,51 +4,62 @@ Day 9: Smoke Basin part 1 & 2
 
 https://adventofcode.com/2021/day/9
 """
-def getLavaMap():
-  with open("2021/D9/d9-input.txt") as f:
+import os
+
+def readInput(filename):
+  with open(os.path.join("2021", "D09", filename)) as f:
     lines = f.readlines()
-  
-  caves = [list(l.strip("\n")) for l in lines]
-  return caves
+  caves = [tuple(l.strip("\n")) for l in lines]
+  return tuple(caves)
 
-caves = getLavaMap()
-hight = len(caves)
-width = len(caves[0])
+class CaveMap:
 
-def getPoint(x, y):
-  if y < 0 or y >= hight or x < 0 or x >= width:
-    return 9
-  return int(caves[y][x])
+  def __init__(self, caves):
+    self._caves = caves
+    self.height = len(caves)
+    self.width = len(caves[0])
 
-def calculateBasinSize(x, y, visited):
-  size = 1
-  visited.add((x,y))
-  # recurse in every direction
-  if (x, y-1) not in visited and getPoint(x, y-1) != 9:
-    size += calculateBasinSize(x, y-1, visited)
-  if (x+1, y) not in visited and getPoint(x+1, y) != 9:
-    size += calculateBasinSize(x+1, y, visited)
-  if (x, y+1) not in visited and getPoint(x, y+1) != 9:
-    size += calculateBasinSize(x, y+1, visited)
-  if (x-1, y) not in visited and getPoint(x-1, y) != 9:
-    size += calculateBasinSize(x-1, y, visited)
-  return size
+  def getPoint(self, x, y):
+    if y < 0 or y >= self.height or x < 0 or x >= self.width:
+      return 9
+    return int(self._caves[y][x])
 
-lowestPoints = []
-for y in range(hight):
-  for x in range(width):
-    p = getPoint(x, y)
-    points = []
-    points.append(getPoint(x, y-1))
-    points.append(getPoint(x+1, y))
-    points.append(getPoint(x, y+1))
-    points.append(getPoint(x-1, y))
-    m = min(points)
-    if p < m:
-      # in a basin, calculate the size
-      size = calculateBasinSize(x,y, visited=set([(x,y)]))
-      lowestPoints.append((x,y,p,size))
+  def calculateBasinSize(self, x, y, visited):
+    size = 1
+    visited.add((x, y))
+    # recurse in every direction
+    if (x, y - 1) not in visited and self.getPoint(x, y - 1) != 9:
+      size += self.calculateBasinSize(x, y - 1, visited)
+    if (x + 1, y) not in visited and self.getPoint(x + 1, y) != 9:
+      size += self.calculateBasinSize(x + 1, y, visited)
+    if (x, y + 1) not in visited and self.getPoint(x, y + 1) != 9:
+      size += self.calculateBasinSize(x, y + 1, visited)
+    if (x - 1, y) not in visited and self.getPoint(x - 1, y) != 9:
+      size += self.calculateBasinSize(x - 1, y, visited)
+    return size
 
-riskLevelSum = sum([i[3] for i in lowestPoints]) + len(lowestPoints)
-bigestBasin = sorted(lowestPoints, reverse=True, key=lambda x: x[3])
-print(riskLevelSum, bigestBasin[0][3]*bigestBasin[1][3]*bigestBasin[2][3], bigestBasin[:3])
+def solve(filename):
+  caves = readInput(filename)
+  caveMap = CaveMap(caves)
+  lowestPoints = []
+  for y in range(caveMap.height):
+    for x in range(caveMap.width):
+      p = caveMap.getPoint(x, y)
+      m = min(caveMap.getPoint(x, y - 1), caveMap.getPoint(x + 1, y), \
+                caveMap.getPoint(x, y + 1), caveMap.getPoint(x - 1, y))
+      if p < m:
+        # in a basin, calculate the size
+        size = caveMap.calculateBasinSize(x, y, visited=set([(x, y)]))
+        lowestPoints.append((x, y, p + 1, size))
+
+  riskLevelSum = sum((i[2] for i in lowestPoints))
+  bigestBasin = sorted(lowestPoints, reverse=True, key=lambda x: x[3])
+
+  r = 1
+  for p in bigestBasin[:3]:
+    r *= p[3]
+
+  return riskLevelSum, r
+
+print(f"Part 1 & 2 for sample: {solve('d9-sample1.txt')}")
+print(f"Part 1 & 2 for puzzle input: {solve('d9-input.txt')}")
