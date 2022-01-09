3c314350d28b5cc49552a06f63bc719f3978dc73
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-01-09 17:44:24 +0800

    Advent Of Code 2021 Day 7 part 1 & 2

--- 2021/D07/d7.py
+++ 2021/D07/d7.py
@@ -4,19 +4,27 @@ Day 7: The Treachery of Whales part 1 & 2
 
 https://adventofcode.com/2021/day/7
 """
-infinity = float("inf")
+import os
 
-def findBestPosition(crabs, fuelCostFct = lambda x: x):
+INFINITY = float("inf")
+
+def readInput(filename):
+  with open(os.path.join("2021", "D07", filename)) as f:
+    lines = f.readlines()
+  input = [int(i) for i in lines[0].strip('\n').split(',')]
+  return tuple(input)
+
+def solve(crabs, fuelCostFct=lambda x: x):
   highestPos = max(crabs)
   lowestPos = min(crabs)
   bestPosition = 0
-  minFuel = infinity
-  for pos in range(lowestPos, highestPos+1):
+  minFuel = INFINITY
+  for pos in range(lowestPos, highestPos + 1):
     totalFuel = 0
     for c in crabs:
       fuel = abs(c - pos)
       totalFuel += fuelCostFct(fuel)
-      
+
       if totalFuel > minFuel:
         break
 
@@ -26,15 +34,12 @@ def findBestPosition(crabs, fuelCostFct = lambda x: x):
 
   return bestPosition, minFuel
 
+sample = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+input = readInput("d7-input.txt")
 
-sample = [16,1,2,0,4,2,7,1,2,14]
-with open("2021/D7/d7-input.txt") as f:
-  lines = f.readlines()
-input = [int(i) for i in lines[0].strip('\n').split(',')]
+print(f"Part 1 sample: {solve(sample)}")
+print(f"Part 1 puzzle input: {solve(input)}")
 
-print("Part 1 sample:",findBestPosition(sample))
-print("Part 1 puzzle:",findBestPosition(input))
-
-# for part 2 fuel cost now evolve as divergent serie 
-print("Part 2 sample:",findBestPosition(sample, fuelCostFct=lambda x: (x*(x+1)/2)))
-print("Part 2 puzzle:",findBestPosition(input, fuelCostFct=lambda x: (x*(x+1)/2)))
+# for part 2 fuel cost now evolve as divergent serie
+print(f"Part 2 sample: {solve(sample, fuelCostFct=lambda x: (x * (x + 1) / 2))}")
+print(f"Part 2 puzzle input: {solve(input, fuelCostFct=lambda x: (x * (x + 1) / 2))}")
