337ea8b1f359cb4555536935168aa99fb948bf28
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-01-10 04:13:28 +0800

    Advent Of Code 2021 Day 17 part 1 & 2

--- 2021/D17/d17.py
+++ 2021/D17/d17.py
@@ -4,23 +4,24 @@ Day 17: Trick Shot part 1 & 2
 
 https://adventofcode.com/2021/day/17 
 """
+
 def part1(targetX, targetY):
   v = abs(targetY[1]) - 1
-  y = int(v*(v+1)/2) # divergent serie formulat n(n+1)/2
+  y = int(v * (v + 1) / 2)  # divergent serie formulat n(n+1)/2
   return y
 
 # brut force for part 2 ....
 def fire(velX, velY, targetX, targetY):
   x, y = 0, 0
   while velX > 0 or y >= targetY[1]:
-    if x >= targetX[0] and x <= targetX[1] and y<= targetY[0] and y>= targetY[1]:
+    if x >= targetX[0] and x <= targetX[1] and y <= targetY[0] and y >= targetY[1]:
       return True
 
     if y < targetY[1]:
       return False
     x += velX
     y += velY
-    velX = max(0, velX-1)
+    velX = max(0, velX - 1)
     velY -= 1
   return False
 
@@ -28,17 +29,16 @@ def part2(targetX, targetY):
   velYRange = (targetY[1], abs(targetY[1]) - 1)
   velXRange = (1, targetX[1])
   velocities = []
-  for velx in range(velXRange[0], velXRange[1]+1, 1):
-    for vely in range(velYRange[0], velYRange[1]+1, 1):
+  for velx in range(velXRange[0], velXRange[1] + 1, 1):
+    for vely in range(velYRange[0], velYRange[1] + 1, 1):
       if fire(velx, vely, targetX, targetY):
         velocities.append((velx, vely))
-
   return len(velocities)
 
 # example
-print(f'Part1: {part1((20, 30),(-5, -10))}')
-print(f'Part2: {part2((20, 30),(-5, -10))}')
+print(f'Part 1 for sample: {part1((20, 30),(-5, -10))}')
+print(f'Part 2 for sample: {part2((20, 30),(-5, -10))}')
 
 # my puzzle input
-print(f'Part1: {part1((257, 286),(-57, -101))}')
-print(f'Part2: {part2((257, 286),(-57, -101))}')
+print(f'Part 1 for puzzle input: {part1((257, 286),(-57, -101))}')
+print(f'Part 2 for puzzle input: {part2((257, 286),(-57, -101))}')
