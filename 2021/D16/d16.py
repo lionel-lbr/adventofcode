3f3b5a3905a9fbc624c1f7925512e1d1f3f871a4
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-01-10 04:08:26 +0800

    Advent Of Code 2021 Day 16 part 1 & 2

--- 2021/D16/d16.py
+++ 2021/D16/d16.py
@@ -4,7 +4,15 @@ Day 16: Packet Decoder part 2
 
 https://adventofcode.com/2021/day/16 
 """
+import os
+
 OPERATORS = ['sum', "pro", "min", "max", "T", "grt", "let", "equ"]
+
+def readInput(filename):
+  with open(os.path.join("2021", "D16", filename)) as f:
+    lines = f.readlines()
+  input = lines[0].strip('\n')
+  return input
 
 def convertToBitString(hexStr):
   bits = ""
@@ -13,10 +21,11 @@ def convertToBitString(hexStr):
   return bits
 
 class PacketParser:
+
   def __init__(self, bitsStr):
     self._bitsStr = bitsStr
     self._bitsStrLen = len(self._bitsStr)
-    self._version = 0
+    self.version = 0
     self._i = 0
     self.values = []
 
@@ -35,13 +44,13 @@ class PacketParser:
     while (self._bitsStrLen - self._i) > 6:
       self.parseOnePacket()
 
-  def _readBits(self, nbrOfBits):
-    s = self._bitsStr[self._i:self._i+nbrOfBits]
-    self._i += nbrOfBits
+  def _readBits(self, bitsCount):
+    s = self._bitsStr[self._i:self._i + bitsCount]
+    self._i += bitsCount
     return s
 
   def _readVersion(self):
-    self._version += int(self._readBits(3), 2)
+    self.version += int(self._readBits(3), 2)
 
   def _readType(self):
     return int(self._readBits(3), 2)
@@ -59,28 +68,30 @@ class PacketParser:
     if v == '0':
       # read 15 bits length
       l = int(self._readBits(15), 2)
-      bs = self._bitsStr[self._i:self._i+l]
+      bs = self._bitsStr[self._i:self._i + l]
       p = PacketParser(bs)
       p.parse()
       self.values.extend(p.values)
+      self.version += p.version
       self.reduce()
       self._i += l
       return
 
-    # read nbr of operators' args
+    # read number of operators' args
     l = int(self._readBits(11), 2)
     # read and parse every arg of operators, evaluate at the end
-    for j in range(l):
+    for _ in range(l):
       bs = self._bitsStr[self._i:]
       p = PacketParser(bs)
       k = p.parseOnePacket()
-      self.values.append(p.values[0])  
+      self.version += p.version
+      self.values.append(p.values[0])
       self._i += k
     self.reduce()
 
   def reduce(self):
     s = []
-    while len(self.values) > 0:
+    while self.values:
       v = self.values.pop()
       if v == "sum":
         self.values.append(sum(s))
@@ -94,7 +105,7 @@ class PacketParser:
       if v == "min":
         self.values.append(min(s))
         return
-      if v == "max": 
+      if v == "max":
         self.values.append(max(s))
         return
       if v == "grt":
@@ -108,14 +119,13 @@ class PacketParser:
         return
       s.append(v)
 
-with open("2021/D16/d16-input.txt") as f:
-  lines = f.readlines()
-input = lines[0].strip('\n')
-bs = convertToBitString(input)
-P = PacketParser(bs)
-P.parse()
-print("Result:", P.values[0])
+def solve(filename):
+  input = readInput(filename)
+  bs = convertToBitString(input)
+  p = PacketParser(bs)
+  p.parse()
+  return p.version, p.values[0]
 
-
-
-
+version, value = solve("d16-input.txt")
+print(f"Part1: {version}")
+print(f"Part2: {value}")
