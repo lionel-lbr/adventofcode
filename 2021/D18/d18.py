54d4913cdf882603896d863a518c5990619cc8ca
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-01-10 21:49:38 +0800

    Advent Of Code 2021 Day 18 part 1 & 2

--- 2021/D18/d18.py
+++ 2021/D18/d18.py
@@ -5,6 +5,13 @@ Day 18: Snailfish part 1 & 2
 https://adventofcode.com/2021/day/18 
 """
 import os
+from time import perf_counter as pfc
+
+def readInput(filename):
+  with open(os.path.join("2021", "D18", filename)) as f:
+    lines = f.readlines()
+  input = tuple(l.strip('\n') for l in lines)
+  return input
 
 class Snailfish:
   def __init__(self, lst):
@@ -18,11 +25,11 @@ class Snailfish:
     res = []
     while i < len(self._lst):
       if self._lst[i] == "[":
-        l, i = self.getList(i+1)
+        l, i = self.getList(i + 1)
         res.append(l)
         continue
       if self._lst[i] == "]":
-        return res, i+1
+        return res, i + 1
       res.append(self._lst[i])
       i += 1
     return res[0]
@@ -35,13 +42,12 @@ class Snailfish:
       if c.isdigit():
         digit += c
         continue
-      if len(digit) > 0:
+      if digit:
         res.append(int(digit))
         digit = ""
       res.append(c)
-    while "," in res:
-      res.remove(",")
-    return res
+    # remove all comma
+    return list(filter(lambda x: x != ",", res))
 
   def explodeAndReduce(self):
     while True:
@@ -49,68 +55,53 @@ class Snailfish:
         pass
       if not self.reduce():
         break
-  
+
   def reduce(self):
-    i = 0
-    while i < len(self._lst):
-      c = self._lst[i]
+    for i, c in enumerate(self._lst):
       if isinstance(c, int) and c >= 10:
-        n = self._lst[i]
-        l = int(n/2)
-        r = n - l
-        right = self._lst[i+1:]
-        self._lst = list(self._lst[:i])
-        self._lst.extend(["[",l,r,"]"])
-        self._lst.extend(right)
+        ln = int(c / 2)
+        rn = c - ln
+        right = self._lst[i + 1:]
+        self._lst = self._lst[:i] + ["[", ln, rn, "]"] + right
         return True
-      i += 1
     return False
 
   def explode(self):
     leftDigitIndex = -1
     deep = 0
-    i = 0
-    while i < len(self._lst):
-      c = self._lst[i]
+    for i, c in enumerate(self._lst):
       if c == "[":
         deep += 1
         if deep >= 5 and isinstance(self._lst[i+1], int) \
                       and isinstance(self._lst[i+2], int):
           # propagate change to left
           if leftDigitIndex > 0:
-            self._lst[leftDigitIndex] += self._lst[i+1]
-          
+            self._lst[leftDigitIndex] += self._lst[i + 1]
+
           # remove the pair and replace by 0
-          rightNumber = self._lst[i+2]
-          right = self._lst[i+4:]
-          self._lst = list(self._lst[0:i])
-          self._lst.append(0)
-          self._lst.extend(right)
+          rightNumber = self._lst[i + 2]
+          right = self._lst[i + 4:]
+          self._lst = self._lst[:i] + [0] + right
 
           # search for the next number on the right
-          i += 1
-          while i < len(self._lst): 
-            if isinstance(self._lst[i], int):
-              self._lst[i] += rightNumber
-              break 
-            i+=1
+          for j in range(i + 1, len(self._lst)):
+            if isinstance(self._lst[j], int):
+              self._lst[j] += rightNumber
+              break
           return True
       elif c == "]":
         deep -= 1
       else:
         leftDigitIndex = i
-      i += 1
     return False
 
   def add(self, lst):
-    self._lst.insert(0,'[')
-    self._lst.extend(self.listToStingList(lst))
-    self._lst.append(']')
+    self._lst = ['['] + self._lst + self.listToStingList(lst) + [']']
     self.explodeAndReduce()
 
   def magnitude(self, lst=None):
     if lst is None:
-      lst = self.getList()  
+      lst = self.getList()
     if isinstance(lst[0], int):
       lm = lst[0] * 3
     else:
@@ -122,23 +113,16 @@ class Snailfish:
     return lm + rm
 
 def part1(filename):
-  with open(os.path.join("2021","D18",filename)) as f:
-    lines = f.readlines()
-  input = [l.strip('\n') for l in lines]
-  sf = None
-  for i in input:
-    if sf is None:
-      sf = Snailfish(i)
-      continue
+  input = readInput(filename)
+  start = pfc()
+  sf = Snailfish(input[0])
+  for i in input[1:]:
     sf.add(i)
-  print("Part 1 result for %s"%filename)
-  print(sf)
-  print(sf.magnitude())
+  return sf.magnitude(), pfc() - start
 
 def part2(filename):
-  with open(os.path.join("2021","D18",filename)) as f:
-    lines = f.readlines()
-  input = [l.strip('\n') for l in lines]
+  input = readInput(filename)
+  start = pfc()
   maxMagnitude = 0
   for i, op1 in enumerate(input):
     for j, op2 in enumerate(input):
@@ -148,16 +132,18 @@ def part2(filename):
       sf.add(op2)
       m = sf.magnitude()
       maxMagnitude = max(maxMagnitude, m)
-      print(sf)
-      print("%d - %d"%(maxMagnitude, m))
+  return maxMagnitude, pfc() - start
 
-  print("Part 2 result for %s"%filename)
-  print("Max magnitude:", maxMagnitude)
-
-part1("d18-sample1.txt")
-part1("d18-sample2.txt")
-part1("d18-input.txt")
-
-part2("d18-sample1.txt")
-part2("d18-sample2.txt")
-part2("d18-input.txt")
+result, time = part1('d18-sample1.txt')
+print(f"Part 1 for sample 1: {result}, time:{time:.2f} sec")
+result, time = part1('d18-sample2.txt')
+print(f"Part 1 for sample 2: {result}, time:{time:.2f} sec")
+result, time = part1('d18-input.txt')
+print(f"Part 1 for puzzle input: {result}, time:{time:.2f} sec")
+print("")
+result, time = part2('d18-sample1.txt')
+print(f"Part 2 for sample 1: {result}, time:{time:.2f} sec")
+result, time = part2('d18-sample2.txt')
+print(f"Part 2 for sample 1: {result}, time:{time:.2f} sec")
+result, time = part2('d18-input.txt')
+print(f"Part 2 for puzzle input: {result}, time:{time:.2f} sec")
