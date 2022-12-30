71452ce2b7ac7a266c8546c7b2643debc1f3330b
lionel-lbr <lionel-lbr@users.noreply.github.com>
2022-12-30 13:07:41 +0800

    AOC 2022 D17 polish code

--- 2022/D17/d17.js
+++ 2022/D17/d17.js
@@ -1,6 +1,6 @@
 /*
 Advent Of Code 2022
-Day 17: xx part 1 & 2
+Day 17: Pyroclastic Flow part 1 & 2
 
 https://adventofcode.com/2022/day/17
 */
@@ -12,27 +12,15 @@ const YEAR = '2022';
 const DAY = '17';
 
 function readInput(filename) {
-  const readRawIntput = () => {
-    const data = fs.readFileSync(path.join(`${YEAR}`, `D${DAY}`, filename));
-    const lines = data
-      .toString()
-      .split(`\n`)
-      .filter((l) => l);
-
-    return lines;
-  };
-
-  const parseALine = (line) => {};
-
-  try {
-    const lines = readRawIntput();
-    return lines[0].split('');
-  } catch (err) {
-    console.error(err);
-  }
+  return fs
+    .readFileSync(path.join(`${YEAR}`, `D${DAY}`, filename))
+    .toString()
+    .split(`\n`)
+    .filter((l) => l)
+    .pop()
+    .split('');
 }
 
-const EMPTY = '|.......|'.split('');
 const ROCKS = [
   [
     { x: 0, y: 0 },
@@ -46,26 +34,26 @@ const ROCKS = [
     { x: 1, y: 1 },
     { x: 2, y: 1 },
     { x: 1, y: 2 },
-  ],
+  ].reverse(),
   [
     { x: 2, y: 0 },
     { x: 2, y: 1 },
     { x: 0, y: 2 },
     { x: 1, y: 2 },
     { x: 2, y: 2 },
-  ],
+  ].reverse(),
   [
     { x: 0, y: 0 },
     { x: 0, y: 1 },
     { x: 0, y: 2 },
     { x: 0, y: 3 },
-  ],
+  ].reverse(),
   [
     { x: 0, y: 0 },
     { x: 1, y: 0 },
     { x: 0, y: 1 },
     { x: 1, y: 1 },
-  ],
+  ].reverse(),
 ];
 
 function solve(input, MAX_ROCK_COUNT) {
@@ -97,33 +85,31 @@ function solve(input, MAX_ROCK_COUNT) {
 
   let count = 0;
   let gazIndex = -1;
+  let topRowIndex = 0;
   let height = 0;
-  let topRow = 0;
-
   while (count < MAX_ROCK_COUNT) {
-    // insert rock
+    // add a new rock
     const rock = ROCKS[count % ROCKS.length];
-    const needed = rock[rock.length - 1].y + 1 + 3;
-    if (needed < topRow) {
-      map.splice(0, topRow - needed);
-      topRow -= topRow - needed;
-    } else if (needed > topRow) {
-      const blankLInes = new Array(needed - topRow).fill(1).map((v) => [...EMPTY]);
-      map.splice(0, 0, ...blankLInes);
-      topRow += blankLInes.length;
-    }
+    const newBlankRowCount = rock[0].y + 1 + 3;
+    if (newBlankRowCount < topRowIndex) {
+      rockY = topRowIndex - newBlankRowCount;
+    } else if (newBlankRowCount > topRowIndex) {
+      for (let i = 0; i < newBlankRowCount - topRowIndex; i++) {
+        map.splice(0, 0, '#.......#'.split(''));
+      }
+      topRowIndex += newBlankRowCount - topRowIndex;
+      rockY = 0;
+    } else rockY = 0;
 
-    // map.splice(0, 0, ...[EMPTY.split(''), EMPTY.split(''), EMPTY.split('')]);
-    // map.splice(0, 0, ...[EMPTY.split(''), EMPTY.split(''), EMPTY.split('')]);
-    // topRow += 6;
-
-    rockY = 0;
     rockX = 3;
     while (1) {
       gazIndex = (gazIndex + 1) % input.length;
+      if (gazIndex === 0) {
+        console.log('gazIndex is 0');
+      }
       const direction = input[gazIndex];
       if (direction === '>' && canMoveRight(rock)) rockX += 1;
-      if (direction === '<' && canMoveLeft(rock)) rockX -= 1;
+      else if (direction === '<' && canMoveLeft(rock)) rockX -= 1;
       if (canMoveDown(rock)) {
         rockY += 1;
       } else {
@@ -131,25 +117,26 @@ function solve(input, MAX_ROCK_COUNT) {
         for ({ x, y } of rock) {
           map[rockY + y][rockX + x] = '#';
         }
+        console.log(`Freeze rock - gazIndex ${gazIndex} count ${(count + 1) % ROCKS.length}`);
         break;
       }
     }
 
-    //part 2 ...
-    for ({ y } of rock) {
-      if (map[rockY + y].slice(1, -1).every((c) => c === '#')) {
-        const index = rockY + y + 1;
-        height += map.length - index - 1;
-        map.splice(index, map.length - index - 1);
-        break;
-      }
-    }
-
-    topRow = Math.min(topRow, rockY);
+    topRowIndex = Math.min(topRowIndex, rockY);
     count += 1;
+
+    if (gazIndex === 17 && count % ROCKS.length === 0) {
+      const cycleCount = Math.floor(MAX_ROCK_COUNT / count);
+      const cycleReminderCount = MAX_ROCK_COUNT - cycleCount * count;
+      height += (map.length - 1 - topRowIndex) * cycleCount;
+      count = cycleCount * count;
+      console.log(`${cycleCount} - ${cycleReminderCount}`);
+      map.splice(0, map.length - 1);
+      topRowIndex = 0;
+    }
   }
 
-  return map.length - 1 + height - topRow;
+  return map.length + height - topRowIndex;
 }
 
 //const input = readInput(`d${DAY}-sample.txt`);
