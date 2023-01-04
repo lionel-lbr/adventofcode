bf14006844c06aed4aac1367494a04cc8d249918
lionel-lbr <lionel-lbr@users.noreply.github.com>
2023-01-04 21:28:23 +0800

    AOC 2022 D18 Part 1 & 2 JS solutions

--- 2022/D18/d18.js
+++ 2022/D18/d18.js
@@ -1,123 +1,122 @@
 /*
 Advent Of Code 2022
-Day 18: xx part 1 & 2
+Day 18: Boiling Boulders part 1 & 2
 
 https://adventofcode.com/2022/day/18
 */
 
 const fs = require('fs');
 const path = require('path');
-// const { Terminal } = require('command-line-draw');
-// const terminal = new Terminal();
-
-const terminal = {
-  write: () => {},
-};
 
 const YEAR = '2022';
 const DAY = '18';
 
 function readInput(filename) {
-  const readRawIntput = () => {
-    const data = fs.readFileSync(path.join(`${YEAR}`, `D${DAY}`, filename));
-    const lines = data
-      .toString()
-      .split(`\n`)
-      .filter((l) => l);
-
-    return lines;
-  };
-
-  const parseALine = (line) => {};
-
-  try {
-    const lines = readRawIntput();
-    return lines.map((l) => l.split(',').map((i) => Number(i))).map((c) => ({ x: c[0], y: c[1], z: c[2] }));
-  } catch (err) {
-    console.error(err);
-  }
+  return fs
+    .readFileSync(path.join(`${YEAR}`, `D${DAY}`, filename))
+    .toString()
+    .split(`\n`)
+    .filter((l) => l)
+    .map((l) => l.split(',').map((i) => Number(i)))
+    .map((c) => ({ x: c[0], y: c[1], z: c[2] }));
 }
 
-function part1(input) {
-  let connectedSide = 00;
-  for (let i = 0; i < input.length - 1; i++) {
-    const c1 = input[i];
-    for (let j = i + 1; j < input.length; j++) {
-      const c2 = input[j];
+function part1(droplets) {
+  const startTime = performance.now();
+
+  let connectedSides = 0;
+  for (let i = 0; i < droplets.length - 1; i++) {
+    const c1 = droplets[i];
+    for (let j = i + 1; j < droplets.length; j++) {
+      const c2 = droplets[j];
       if (c1.x === c2.x && c1.y === c2.y && (c1.z === c2.z + 1 || c1.z === c2.z - 1)) {
-        connectedSide += 2;
+        connectedSides += 2;
         continue;
       }
       if (c1.x === c2.x && c1.z === c2.z && (c1.y === c2.y + 1 || c1.y === c2.y - 1)) {
-        connectedSide += 2;
+        connectedSides += 2;
         continue;
       }
       if (c1.y === c2.y && c1.z === c2.z && (c1.x === c2.x + 1 || c1.x === c2.x - 1)) {
-        connectedSide += 2;
+        connectedSides += 2;
         continue;
       }
     }
   }
-  return input.length * 6 - connectedSide;
+  console.log(`Execution time: ${(performance.now() - startTime) / 1000}`);
+  return droplets.length * 6 - connectedSides;
 }
 
-const LETTER = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-function part2(input) {
-  const zMap = new Map();
-  for (c of input) {
-    if (!zMap.has(c.z)) zMap.set(c.z, new Map());
-    const yMap = zMap.get(c.z);
-    if (!yMap.has(c.y)) yMap.set(c.y, []);
-    yMap.get(c.y).push(c.x);
-  }
+function part2(droplets) {
+  const startTime = performance.now();
+  // get max values of each dimentions
+  const xyzMax = droplets.reduce(
+    (d1, d2) => ({ x: Math.max(d1.x, d2.x), y: Math.max(d1.y, d2.y), z: Math.max(d1.z, d2.z) }),
+    { x: -Infinity, y: -Infinity, z: -Infinity }
+  );
 
-  const getPoint = (x, y, z) => {
-    if (!zMap.has(z)) return false;
-    const yMap = zMap.get(z);
-    if (!yMap.has(y)) return false;
-    const l = yMap.get(y);
-    const i = l.findIndex((v) => v === x);
-    if (i === -1) return false;
-    return true;
+  // create 3D map
+  const maps = new Array(xyzMax.z + 3)
+    .fill('.')
+    .map(() => new Array(xyzMax.y + 3).fill('.').map(() => new Array(xyzMax.x + 3).fill('.')));
+
+  // place droplets
+  droplets.forEach(({ x, y, z }) => (maps[z + 1][y + 1][x + 1] = '#'));
+
+  const getDroplet = ({ x, y, z }) => {
+    if (x < 0 || y < 0 || z < 0 || x >= maps[0][0].length || y >= maps[0].length || z >= maps.length) return null;
+    return maps[z][y][x];
   };
 
-  const holes = new Set();
-  for (z of [...zMap.keys()]) {
-    const yMap = zMap.get(z);
-    for (y of [...yMap.keys()]) {
-      const xList = yMap.get(y);
-      for (let i = 0; i < xList.length - 1; i++) {
-        const x = xList[i];
-        if (x + 2 === xList[i + 1]) {
-          // found a hole
-          if (
-            getPoint(x + 1, y - 1, z) &&
-            getPoint(x + 1, y + 1, z) &&
-            getPoint(x + 1, y, z + 1) &&
-            getPoint(x + 1, y, z - 1)
-          ) {
-            holes.add(`${x + 1}:${y}:${z}`);
-          }
-        }
-      }
-    }
-    // for ({ x, y } of l) {
-    //   terminal.write(LETTER[k], x, y);
-    // }
-  }
-  // const l = zMap.get(3);
-  // l.sort((a, b) => (a.y === b.y ? a.x - b.x : a.y - b.y));
-  // for ({ x, y } of l) {
-  //   terminal.write('X', x, y);
-  // }
+  const getNeighbours = ({ x, y, z }) => {
+    const n = [];
+    const top = { x, y: y - 1, z };
+    const right = { x: x + 1, y, z };
+    const bottom = { x, y: y + 1, z };
+    const left = { x: x - 1, y, z };
+    const below = { x, y, z: z + 1 };
+    const above = { x, y, z: z - 1 };
 
-  // terminal.write('X', x, y);
-  return 0;
+    if (getDroplet(below) === '.') n.push(below);
+    if (getDroplet(right) === '.') n.push(right);
+    if (getDroplet(bottom) === '.') n.push(bottom);
+    if (getDroplet(top) === '.') n.push(top);
+    if (getDroplet(left) === '.') n.push(left);
+    if (getDroplet(above) === '.') n.push(above);
+    return n;
+  };
+
+  const countNeighbourFaces = ({ x, y, z }) => {
+    let count = 0;
+    count += getDroplet({ x, y: y - 1, z }) === '#';
+    count += getDroplet({ x: x + 1, y, z }) === '#';
+    count += getDroplet({ x, y: y + 1, z }) === '#';
+    count += getDroplet({ x: x - 1, y, z }) === '#';
+    count += getDroplet({ x, y, z: z + 1 }) === '#';
+    count += getDroplet({ x, y, z: z - 1 }) === '#';
+    return count;
+  };
+
+  const queue = [{ x: 0, y: 0, z: 0 }];
+  let faces = 0;
+  while (queue.length > 0) {
+    const p = queue.pop();
+    // if visited continue
+    if (getDroplet(p) === '0') continue;
+    maps[p.z][p.y][p.x] = '0';
+    faces += countNeighbourFaces(p);
+    const neighbours = getNeighbours(p);
+    for (n of neighbours) {
+      if (getDroplet(n) === '0') continue;
+      queue.push(n);
+    }
+  }
+
+  console.log(`Execution time: ${(performance.now() - startTime) / 1000}`);
+  return faces;
 }
 
 //const input = readInput(`d${DAY}-sample.txt`);
 const input = readInput(`d${DAY}-input.txt`);
-input.sort((a, b) => (a.z === b.z ? (a.y === b.y ? a.x - b.x : a.y - b.y) : a.z - b.z));
 console.log(`Part 1: ${part1(input)}`);
-part2(input);
-// console.log(`Part 2: ${part2(input)}`);
+console.log(`Part 2: ${part2(input)}`);
